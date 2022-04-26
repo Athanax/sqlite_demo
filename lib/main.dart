f87cd04fd3f638105d65cd09928ce717d1947753
Athanas Wambua <athanaswambua@zeed-2.local>
2022-04-26 09:59:28 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,168 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() async {
+    test();
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void test() {
+    List<User>? users = [];
+    users = User().where(k: 'name', v: 'wambua').get() as List<User>;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class Grocery {
+  final int? id;
+  final String name;
+
+  Grocery({this.id, required this.name});
+
+  factory Grocery.fromMap(Map<String, dynamic> json) => Grocery(
+        id: json['id'],
+        name: json['name'],
+      );
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+    };
+  }
+}
+
+class DatabaseHelper {
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+  static Database? _database;
+  Future<Database> get database async => _database ??= await _initDatabase();
+
+  Future<Database> _initDatabase() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, 'groceries.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE groceries(
+          id INTEGER PRIMARY KEY,
+          name TEXT
+      )
+      ''');
+  }
+
+  Future<List<Grocery>> getGroceries() async {
+    Database db = await instance.database;
+    var groceries = await db.query('groceries', orderBy: 'name');
+    List<Grocery> groceryList = groceries.isNotEmpty
+        ? groceries.map((c) => Grocery.fromMap(c)).toList()
+        : [];
+    return groceryList;
+  }
+
+  Future<int> add(Grocery grocery) async {
+    Database db = await instance.database;
+    return await db.insert('groceries', grocery.toMap());
+  }
+
+  Future<int> remove(int id) async {
+    Database db = await instance.database;
+    return await db.delete('groceries',
+        where: 'id = ?&name = ?', whereArgs: [id, 'wambua']);
+  }
+
+  Future<int> update(Grocery grocery) async {
+    Database db = await instance.database;
+    return await db.update('groceries', grocery.toMap(),
+        where: "id = ?", whereArgs: [grocery.id]);
+  }
+}
+
+class Builder {
+  //
+  Builder where({required String k, String op = '=', required dynamic v}) {
+    return this;
+  }
+
+  List<Builder> get() {
+    return [];
+  }
+}
+
+class User extends Builder {
+  int? id;
+  String? name;
+  User({this.id, this.name});
+}
